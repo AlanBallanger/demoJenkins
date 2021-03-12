bcedf750c0bbdbe1b58be118ee62ad6278857d33
Alan Ballanger <alan.ballanger@live.fr>
2021-03-12 11:38:18 +0100

    added all calc functions

--- src/main/java/com/epsi/calc/Application.java
+++ src/main/java/com/epsi/calc/Application.java
@@ -1,7 +1,15 @@
 package com.epsi.calc;
 
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 public class Application {
   public static void main(String[] args) {
     System.out.println("Hello world");
+
+    Scanner myObj = new Scanner(System.in, Charset.defaultCharset());
+    System.out.println("Enter a random number");
+    String number = myObj.nextLine();
+    System.out.println("You chose: " + number);
   }
 }
